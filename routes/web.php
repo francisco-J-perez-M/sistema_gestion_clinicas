787339590ed133bc9d2492359b09a3f31abc5562
francisco-J-perez-M <franperez159264@gmail.com>
2024-10-22 09:13:26 -0600

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PacienteController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PacienteController::class, 'index']);
+
+// Ruta para listar pacientes
+Route::get('pacientes', [PacienteController::class, 'index'])->name('pacientes.index');
+
+// Ruta para mostrar el formulario de creación de paciente
+Route::get('pacientes/create', [PacienteController::class, 'create'])->name('pacientes.create');
+
+// Ruta para almacenar un nuevo paciente
+Route::post('pacientes', [PacienteController::class, 'store'])->name('pacientes.store');
+
+// Ruta para mostrar un paciente específico
+Route::get('pacientes/{paciente}', [PacienteController::class, 'show'])->name('pacientes.show');
+
+// Ruta para mostrar el formulario de edición de un paciente
+Route::get('pacientes/{paciente}/edit', [PacienteController::class, 'edit'])->name('pacientes.edit');
+
+// Ruta para actualizar un paciente existente
+Route::put('pacientes/{paciente}', [PacienteController::class, 'update'])->name('pacientes.update');
+
+// Ruta para eliminar un paciente
+Route::delete('pacientes/{paciente}', [PacienteController::class, 'destroy'])->name('pacientes.destroy');
