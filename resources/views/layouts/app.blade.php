fbda858787554bdde28d8393f2a6716046162e9c
francisco-J-perez-M <franperez159264@gmail.com>
2024-10-27 11:51:11 -0600

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,14 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'Pacientes')</title>
+    <title>@yield('title', 'Polaris Clinic')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">polaris clinic</a>
+            <a class="navbar-brand" href="#">Polaris Clinic</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
@@ -20,7 +20,7 @@
                         <a class="nav-link active" href="{{ route('pacientes.index') }}">Pacientes</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Médicos</a>
+                        <a class="nav-link" href="{{ route('medicos.index') }}">Médicos</a> <!-- Ruta a la lista de médicos -->
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Medicamentos</a>
