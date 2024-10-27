8277da05b529cd12d232162b27dd5ab59dd64655
francisco-J-perez-M <franperez159264@gmail.com>
2024-10-27 15:18:41 -0600

    Add files via upload

--- database/migrations/2024_10_18_174959_medicamentos.php
+++ database/migrations/2024_10_18_174959_medicamentos.php
@@ -8,17 +8,30 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        //
+        Schema::create('medicamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre'); // Nombre del medicamento
+            $table->string('dosis'); // Dosis recomendada
+            $table->string('unidad'); // Unidad de medida (mg, ml, etc.)
+            $table->integer('cantidad'); // Cantidad disponible
+            $table->text('descripcion')->nullable(); // Descripción opcional
+            $table->date('fecha_vencimiento')->nullable(); // Fecha de vencimiento
+            $table->timestamps(); // Campos created_at y updated_at
+        });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        //
+        Schema::dropIfExists('medicamentos');
     }
 };
